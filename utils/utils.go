be01d9768badc822230c33f34d48cbad586da0a3
Hans Erhanto <hanserhantu16@gmail.com>
2023-11-03 13:42:29 +0700

    add function dbtransaction

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 )
@@ -23,4 +24,23 @@ func IsConnected() bool {
 	//	return false
 	//}
 	return true
+}
+
+func DBTransaction(db *sql.DB, txFunc func(*sql.Tx) error) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p) // Rollback Panic
+		} else if err != nil {
+			tx.Rollback() // err is not nill
+		} else {
+			err = tx.Commit() // err is nil
+		}
+	}()
+	err = txFunc(tx)
+	return err
 }
